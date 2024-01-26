2f078c06efc75a89b23026ae0f1aa22db95e4d73
Tomaster134 <tomaster134@gmail.com>
2024-01-26 14:12:15 -0500

    mostly done

---
+++ calculator.py
@@ -0,0 +1,9 @@
+# income: rental income, laundry income, storage, misc income -> duplex with $1000 per unit (2,000 per month)
+
+# expenses: taxes $150, insurance $100, utilities $0 (electirc, water, sewer, garbage, gas), HOA fees $0, lawncare $0, vacancy 5% of income, repairs $100, capital expenditures $100, property management 10%, mortagage $860 -> ($1610)
+
+# cash flow = income minus expenses = $390
+
+# cash on cash ROI - down payment (40,000), closing costs (3000), rehab budget (7000), misc -> 50,000
+
+#Annual Cash Flow (4680) / Total Investment (50,000) = CoC ROI = 9.36%
